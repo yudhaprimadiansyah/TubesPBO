7df66946b1a6ef404c8b30f605aa2b69882bba7f
dodyrifkisuraya <dodyrifkisuraya@gmail.com>
2019-11-26 18:35:13 +0700

    baruuu'

--- tubs/src/com/tubs/gaspol/db/CRUD.java
+++ tubs/src/com/tubs/gaspol/db/CRUD.java
@@ -63,6 +63,129 @@ public class CRUD {
         }
     }
     
+    private void tambahJadwalDosen(String jamMulai, String jamSelesai, int hariKe){
+        if(conn != null){
+            try{
+                String query = "INSERT INTO jadwal_mengajar_dosen(jam_mulai, jam_selesai, hari_ke) VALUES (?,?,?)";
+                PreparedStatement ps = conn.prepareStatement(query);
+                ps.setString(1, jamMulai);
+                ps.setString(2, jamSelesai);
+                ps.setInt(3, hariKe);
+                int hasil = ps.executeUpdate();
+                
+            }catch(SQLException e){
+                Logger.getLogger(CRUD.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+    }
+    
+    private void tambahJadwalSidang(String jamMulai, String jamSelesai, String tanggal, int idPenguji1, int idPenguji2, int idRuang, int idPa, int idPeriode){
+        if(conn != null){
+            try{
+                String query = "INSERT INTO jadwal_sidang(jam_mulai, jam_selesai, tanggal, id_penguji1, id_penguji2, id_ruang, id_pa, id_periode) VALUES (?,?,?,?,?,?,?,?)";
+                PreparedStatement ps = conn.prepareStatement(query);
+                ps.setString(1, jamMulai);
+                ps.setString(2, jamSelesai);
+                ps.setString(3, tanggal);
+                ps.setInt(4, idPenguji1);
+                ps.setInt(5, idPenguji2);
+                ps.setInt(6, idRuang);
+                ps.setInt(7, idPa);
+                ps.setInt(8, idPeriode);
+
+                int hasil = ps.executeUpdate();
+                
+            }catch(SQLException e){
+                Logger.getLogger(CRUD.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+    }
+    
+    private void tambahKeahlian(String namaKeahlian){
+        if(conn != null){
+            try{
+                String query = "INSERT INTO keahlian(nama_keahlian) VALUES (?)";
+                PreparedStatement ps = conn.prepareStatement(query);
+                ps.setString(1, namaKeahlian);
+               
+                int hasil = ps.executeUpdate();
+                
+            }catch(SQLException e){
+                Logger.getLogger(CRUD.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+    }
+    
+    private void tambahPengelola(String username, String password, String nama, String nip){
+        if(conn != null){
+            try{
+                String query = "INSERT INTO pengelola(username, password, nama, nip) VALUES (?,?,?,?)";
+                PreparedStatement ps = conn.prepareStatement(query);
+                ps.setString(1, username);
+                ps.setString(2, password);
+                ps.setString(3, nama);
+                ps.setString(4, nip);          
+
+                int hasil = ps.executeUpdate();
+                
+            }catch(SQLException e){
+                Logger.getLogger(CRUD.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+    }
+    
+    private void tambahPeriodeSidang(String tahun, String tanggalAwal, String tanggalAkhir, String periode){
+        if(conn != null){
+            try{
+                String query = "INSERT INTO periode_sidang(tahun, tanggal_awal, tanggal_akhir, periode_ke) VALUES (?,?,?,?)";
+                PreparedStatement ps = conn.prepareStatement(query);
+                ps.setString(1, tahun);
+                ps.setString(2, tanggalAwal);
+                ps.setString(3, tanggalAkhir);
+                ps.setString(4, periode);          
+
+                int hasil = ps.executeUpdate();
+                
+            }catch(SQLException e){
+                Logger.getLogger(CRUD.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+    }
+    
+    private void tambahPa(String judul, String idDosbing1, String idDosbing2, String keahlian){
+        if(conn != null){
+            try{
+                String query = "INSERT INTO proyek_akhir(judul, id_dosbing1, id_dosbing2, id_keahlian, nim) VALUES (?,?,?,?,?)";
+                PreparedStatement ps = conn.prepareStatement(query);
+                ps.setString(1, judul);
+                ps.setString(2, idDosbing1);
+                ps.setString(3, idDosbing2);
+                ps.setString(4, keahlian);          
+
+                int hasil = ps.executeUpdate();
+                
+            }catch(SQLException e){
+                Logger.getLogger(CRUD.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+    }
+    
+    private void tambahRuang(String nama){
+        if(conn != null){
+            try{
+                String query = "INSERT INTO ruangan(nama) VALUES (?)";
+                PreparedStatement ps = conn.prepareStatement(query);
+                ps.setString(1, nama);
+                
+
+                int hasil = ps.executeUpdate();
+                
+            }catch(SQLException e){
+                Logger.getLogger(CRUD.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+    }
+    
     private void updateDosen(int id, String nama, String email, int idKeahlian, String kodeDosen,String nip){
         if(conn != null){
             try{
@@ -81,5 +204,24 @@ public class CRUD {
             }
         }
     }
+    
+    private void updateMahasiswa(String nama, String email, String nim){
+        if(conn != null){
+            try{
+                String query = "UPDATE mahasiswa nama=?, email=? WHERE nim=?";
+                PreparedStatement ps = conn.prepareStatement(query);
+                ps.setString(1, nama);
+                ps.setString(2, email);
+                ps.setString(3, nim);
+                int hasil = ps.executeUpdate();
+                
+            }catch(SQLException e){
+                Logger.getLogger(CRUD.class.getName()).log(Level.SEVERE, null, e);
+            }
+        }
+    }
+    
+    
+    
     
 }
